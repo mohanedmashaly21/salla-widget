4e8092d7f910689510af767b815213bd2259bbd7
mohaned mashaly <mohanedmashaly@mohaneds-MacBook-Pro.local>
2025-01-17 15:52:24 +0200

    init

---
+++ src/main.js
@@ -0,0 +1,152 @@
+import { CLOSE_ICON, MESSAGE_ICON, styles } from "./assets.js";
+
+class Widget {
+  constructor(position = "bottom-right") {
+    this.position = this.getPosition(position);
+    this.open = false;
+    this.initialize();
+    this.injectStyles();
+  }
+
+  position = "";
+  open = false;
+  widgetContainer = null;
+
+  getPosition(position) {
+    const [vertical, horizontal] = position.split("-");
+    return {
+      [vertical]: "30px",
+      [horizontal]: "30px",
+    };
+  }
+
+  async initialize() {
+    /**
+     * Create and append a div element to the document body
+     */
+    const container = document.createElement("div");
+    container.style.position = "fixed";
+    Object.keys(this.position).forEach(
+      (key) => (container.style[key] = this.position[key])
+    );
+    document.body.appendChild(container);
+
+    /**
+     * Create a button element and give it a class of button__container
+     */
+    const buttonContainer = document.createElement("button");
+    buttonContainer.classList.add("button__container");
+
+    /**
+     * Create a span element for the widget icon, give it a class of 'widget__icon', update it's innerHTML property to an icon which would serve as the widget icon.
+     */
+    const widgetIconElement = document.createElement("span");
+    widgetIconElement.innerHTML = MESSAGE_ICON;
+    widgetIconElement.classList.add("widget__icon");
+    this.widgetIcon = widgetIconElement;
+
+    /**
+     * Create a span element for the close icon, give it a class of 'widget__icon' and 'widget__hidden' which would be removed whenever the widget is closed, update it's innerHTML property to an icon which would serve as the widget icon during that state.
+     */
+    const closeIconElement = document.createElement("span");
+    closeIconElement.innerHTML = CLOSE_ICON;
+    closeIconElement.classList.add("widget__icon", "widget__hidden");
+    this.closeIcon = closeIconElement;
+
+    /**
+     * Append both icons created to the button element and add a `click` event listener on the button to toggle the widget open and close.
+     */
+    buttonContainer.appendChild(this.widgetIcon);
+    buttonContainer.appendChild(this.closeIcon);
+    buttonContainer.addEventListener("click", this.toggleOpen.bind(this));
+
+    /**
+     * Create a container for the widget and add the following classes:- "widget__hidden", "widget__container"
+     */
+    this.widgetContainer = document.createElement("div");
+    this.widgetContainer.classList.add("widget__hidden", "widget__container");
+
+    /**
+     * Invoke the `createWidget()` method
+     */
+    this.createWidgetContent();
+
+    /**
+     * Append the widget's content and the button to the container
+     */
+    container.appendChild(this.widgetContainer);
+    container.appendChild(buttonContainer);
+  }
+
+  createWidgetContent() {
+    this.widgetContainer.innerHTML = `
+        <header class="widget__header">
+            <h3>اكتب رأيك</h3>
+            <p>رأيك يهمنا و يساعدنا لمعرفه شعورك</p>
+        </header>
+
+        <form>
+            <div class="form__field">
+                <label for="review_title">اكتب عنوان رأيك</label>
+                <input
+                type="text"
+                id="review_title"
+                name="review_title"
+                placeholder="اكتب عنوان رأيك"
+                />
+            </div>
+
+            <div class="form__field">
+                <label for="review_message">اكتب  رأيك</label>
+                <textarea
+                id="review_message"
+                name="review_message"
+                placeholder="اكتب رأيك"
+                rows="6"
+                ></textarea>
+            </div>
+
+            <div class="rate">
+            <input type="radio" id="star5" name="rate" value="5" />
+            <label for="star5" title="text">5 stars</label>
+            <input type="radio" id="star4" name="rate" value="4" />
+            <label for="star4" title="text">4 stars</label>
+            <input type="radio" id="star3" name="rate" value="3" />
+            <label for="star3" title="text">3 stars</label>
+            <input type="radio" id="star2" name="rate" value="2" />
+            <label for="star2" title="text">2 stars</label>
+            <input type="radio" id="star1" name="rate" value="1" />
+            <label for="star1" title="text">1 star</label>
+  </div>
+            <button>ارسل  رأيك</button>
+        </form>
+    `;
+  }
+
+  injectStyles() {
+    const styleTag = document.createElement("style");
+    styleTag.innerHTML = styles.replace(/^\s+|\n/gm, "");
+
+    document.head.appendChild(styleTag);
+  }
+
+  toggleOpen() {
+    this.open = !this.open;
+    if (this.open) {
+      this.widgetIcon.classList.add("widget__hidden");
+      this.closeIcon.classList.remove("widget__hidden");
+      this.widgetContainer.classList.remove("widget__hidden");
+    } else {
+      this.createWidgetContent();
+      this.widgetIcon.classList.remove("widget__hidden");
+      this.closeIcon.classList.add("widget__hidden");
+      this.widgetContainer.classList.add("widget__hidden");
+    }
+  }
+}
+
+function initializeWidget() {
+  return new Widget();
+}
+
+initializeWidget();
